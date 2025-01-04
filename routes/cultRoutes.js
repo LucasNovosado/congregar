981c9df2122a8428a96d44d357ce45ca96129065
LucasNovosado <165840126+LucasNovosado@users.noreply.github.com>
2025-01-04 05:32:28 -0300

    Add files via upload

--- routes/cultRoutes.js
+++ routes/cultRoutes.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const path = require('path');
 const cultController = require(path.join(__dirname, '..', 'controllers', 'cultController'));
+const { requireAuth } = require('../controllers/authController');
 const router = express.Router();
 
+// Aplica o middleware de autenticação em todas as rotas
+router.use(requireAuth);
+
+// Rotas existentes, agora protegidas
 router.get('/', cultController.getAllCultData);
-
-// Rota para exibir o formulário de adição
 router.get('/add', (req, res) => {
-    res.render('addCult');
+    res.render('addCult', { user: req.session.user });
 });
-
 router.post('/add', cultController.addCultData);
 router.get('/edit/:id', cultController.getEditForm);
-router.post('/edit/:id', cultController.updateCultData); // Mudou de /update para /edit
+router.post('/edit/:id', cultController.updateCultData);
 router.post('/delete/:id', cultController.deleteCultData);
 
 module.exports = router;
